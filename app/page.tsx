80144bec324e124ff8418180fd2e3bd6b9b9d945
PenguinCabinet <kage2.kage22@gmail.com>
2025-11-26 12:54:00 +0900

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,153 @@
-import Image from "next/image";
+'use client'
+import { useCallback, useEffect, useState } from 'react'
+import Image from "next/image"
+import Kuroshiro from "kuroshiro"
+import KuromojiAnalyzer from "kuroshiro-analyzer-kuromoji"
+import "./globals.css"
+import 'material-symbols'
+
+async function sort_func(kuroshiro, x) {
+  const x_arr = x
+    .split("\n")
+    .filter((e) => e != "")
+  const yomigana_x_arr = await Promise.all(
+    x_arr.map(async (e) => {
+      return kuroshiro.convert(e, { to: "hiragana" })
+    })
+  )
+
+  let yomigana_x_index_arr = yomigana_x_arr.map((e, i) => [i, e])
+
+  yomigana_x_index_arr.sort((a, b) => a[1].localeCompare(b[1], 'ja'))
+
+  return {
+    yomigana: yomigana_x_index_arr.map((e) => e[1]).join("\n"),
+    result: yomigana_x_index_arr.map((e) => x_arr[e[0]]).join("\n"),
+  }
+
+}
 
 export default function Home() {
+
+  const [Out_yomigana, setOut_yomigana] = useState("")
+  const [Out_result, setOut_result] = useState("")
+  const [Out_yomigana_placeholder, setOut_yomigana_placeholder] = useState("")
+  const [Out_result_placeholder, setOut_result_placeholder] = useState("")
+
+  const [kuroshiro, set_kuroshiro] = useState(new Kuroshiro())
+  const [isLoading, setLoading] = useState(true)
+
+  const placeholder = `茨城県
+栃木県
+群馬県
+埼玉県
+千葉県
+東京都
+神奈川県
+山梨県
+長野県
+トウキョウト
+とうきょうと
+ﾄｳｷｮｳﾄ
+12345
+`
+
+  useEffect(() => {
+    kuroshiro.init(new KuromojiAnalyzer({ dictPath: "/dict" })).then((e) => {
+
+      sort_func(kuroshiro, placeholder).then((placeholder_result) => {
+        console.log(placeholder_result)
+        setOut_yomigana_placeholder(placeholder_result.yomigana)
+        setOut_result_placeholder(placeholder_result.result)
+      })
+
+      setLoading(false)
+    })
+  }, [])
+
+  if (isLoading) return <div></div>
+
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main>
+      <h1 className="text-3xl font-bold text-heading my-3"
+      >漢字の読みを考慮した、あいうえお順ソートツール</h1>
+
+      <h2 className='text-2xl font-bold text-heading my-3'>
+        ソートしたいデータ</h2>
+
+      <textarea
+        rows={6}
+        className='bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full p-3.5 shadow-xs placeholder:text-body'
+        onChange={
+          (e) => {
+            const f = async () => {
+              const result = await sort_func(kuroshiro, e.target.value)
+              setOut_yomigana(
+                result.yomigana
+              )
+              setOut_result(result.result)
+
+            }
+            f()
+          }
+        }
+        placeholder={placeholder} />
+      <div className='my-3'>
+        <div className="flex items-center gap-4">
+          <h2 className='text-2xl font-bold text-heading'>
+            ソートしたデータの読み方</h2>
+          <button type="button"
+            onClick={
+              () => {
+                navigator.clipboard.writeText(Out_yomigana)
+              }
+            }
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            <span className="material-symbols-outlined">
+              content_copy
+            </span>
+          </button>
         </div>
-      </main>
-    </div>
-  );
+
+        <textarea
+          rows={6}
+          className='bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full p-3.5 shadow-xs placeholder:text-body'
+          value={Out_yomigana} readOnly={true}
+          placeholder={Out_yomigana_placeholder} />
+      </div>
+
+      <div className='my-3'>
+        <div className="flex items-center gap-4">
+          <h2 className='text-2xl font-bold text-heading'>
+            ソートしたデータ</h2>
+          <button type="button"
+            onClick={
+              () => {
+                navigator.clipboard.writeText(Out_result)
+              }
+            }
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+            <span className="material-symbols-outlined">
+              content_copy
+            </span>
+          </button>
+        </div>
+        <textarea
+          rows={6}
+          className='bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full p-3.5 shadow-xs placeholder:text-body'
+          value={Out_result} readOnly={true}
+          placeholder={Out_result_placeholder} />
+      </div>
+
+
+      <pre>
+
+        <a
+          className='text-blue-500 hover:underline'
+          href='http://example.com/'>Githubリポジトリはこちら</a>
+      </pre>
+    </main>
+  )
 }
